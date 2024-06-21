84bb571e60df3a9a73e6dc35ba3bb7ecda948576
juyeon99 <beive999@naver.com>
2024-06-21 12:48:36 +0900

    Add ifSubMenu()

---
+++ ch05-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
@@ -0,0 +1,69 @@
+package com.ohgiraffers.section01.xml;
+
+import com.ohgiraffers.common.SearchCriteria;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            System.out.println("\n========== MyBatis 동적 SQL (조건문) ==========");
+            System.out.println("1. if 확인하기");
+            System.out.println("2. choose(when, otherwise) 확인하기");
+            System.out.println("3. foreach 확인하기");
+            System.out.println("4. trim(where, set) 확인하기");
+            System.out.println("0. 종료하기");
+            System.out.print("메뉴 선택: ");
+
+            int no = sc.nextInt();
+            sc.nextLine();
+
+            switch (no){
+                case 1: ifSubMenu(); break;
+                case 2:  break;
+                case 3: break;
+                case 4: break;
+                case 0: System.out.println("시스템을 종료합니다."); return;
+            }
+        } while (true);
+    }
+
+    private static void ifSubMenu() {
+        Scanner sc = new Scanner(System.in);
+        MenuService menuService = new MenuService();
+        do {
+            System.out.println("\n========== if 서브메뉴 ==========");
+            System.out.println("1. 원하는 금액대에 적합한 추천메뉴 목록 보여주기");
+            System.out.println("2. 메뉴이름 또는 카테고리 명으로 검색하여 메뉴 목록 보여주기");
+            System.out.println("0. 이전 메뉴로");
+            System.out.print("번호 입력: ");
+            int no = sc.nextInt();
+
+            switch (no){
+                case 1: menuService.selectMenuByPrice(inputPrice()); break;
+                case 2: menuService.searchMenu(inputSearchCriteria()); break;
+                case 0: return;
+            }
+        } while (true);
+    }
+
+    private static int inputPrice(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("검색하실 가격의 최대 금액 입력: ");
+        int price = sc.nextInt();
+
+        return price;
+    }
+
+    private static SearchCriteria inputSearchCriteria() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("검색 기준 입력(name or category): ");
+        String condition = sc.nextLine();
+        System.out.print("검색어 입력: ");
+        String value = sc.nextLine();
+
+        return new SearchCriteria(condition, value);
+    }
+}
